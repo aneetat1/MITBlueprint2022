6ebf03cf35d4e936b11cc60aefd5f5a2a78c4908
Angelina Zhang <55762292+ayz122004@users.noreply.github.com>
2022-02-20 11:04:22 -0800

    change about and styles

--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -26,9 +26,37 @@ const Skeleton = ({ userId, handleLogin, handleLogout }) => {
           onFailure={(err) => console.log(err)}
         />
       )}
+      
+      <body>
       <Link to="about">About</Link>
-      <h1>Home Page</h1>
-      <a href="http://weblab.to/get-started">example link (get started guide)</a>
+        <div class="home-page-content">
+            <div class="landing-page-content">
+                {/* <!-- background image: --> */}
+                <div class="image" id="backgroundImage">
+                    <img src = "https://i.ibb.co/984zbFh/output-onlinepngtools.png" height = "100%" width = "100%"/>
+                </div>
+                
+                {/* <!-- title: --> */}
+                <h1 class="title">campaign.</h1>
+    
+                {/* <!-- subtitle: --> */}
+                <h3 class="subtitle">quickly email your local representatives about current issues.</h3>
+
+                {/* <!-- info button, replace with info button: --> */}
+                <button type="button"> <img src="https://bit.ly/3BF9FD7" height ="80" width="100" /></button>
+    
+                {/* <!-- replaced getRandomImage() with something that brings you back to the home page --> */}
+                <div class="button-container">
+                    <button id="random" onClick="getRandomImage()">campaign.org</button>
+                </div>
+    
+            </div>
+            <div class="outreach-content">
+
+            </div>
+        </div>
+        
+    </body>
     </>
   );
 };
